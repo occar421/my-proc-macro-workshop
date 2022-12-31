3e79d9183b77a0c2e7482aa627650b25c9f3ef59
MasuqaT <occar@hotmail.co.jp>
2023-01-01 04:56:41 +0900

    refactor

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,17 +1,18 @@
 use proc_macro::TokenStream;
 use proc_macro2::Span;
-use quote::quote;
+use quote::{format_ident, quote};
+use syn::__private::TokenStream2;
 use syn::{
-    parse_macro_input, Data, DeriveInput, Field, GenericArgument, Ident, Lit, Meta, NestedMeta,
-    PathArguments, Type, Visibility,
+    parse_macro_input, Attribute, Data, DataStruct, DeriveInput, Field, GenericArgument, Ident,
+    Lit, Meta, NestedMeta, PathArguments, Type, Visibility,
 };
 
 struct AnalyzedField {
     vis: Visibility,
-    ident: Option<Ident>,
+    name: String,
     normalized_type: Type,
     kind: FieldKind,
-    setter_ident: Option<Ident>,
+    setter_name: String,
 }
 
 enum FieldKind {
@@ -23,15 +24,28 @@ enum FieldKind {
 #[proc_macro_derive(Builder, attributes(builder))]
 pub fn derive(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
-    let ident = input.ident;
-    let visibility = input.vis;
-    let builder_ident = Ident::new(&format!("{}Builder", ident), Span::call_site());
+
     let Data::Struct(data) = input.data else {
         return TokenStream::new();
     };
 
-    let fields: Vec<_> = data
-        .fields
+    let fields: Vec<_> = analyze_fields(&data);
+
+    let builder_ident = format_ident!("{}Builder", input.ident);
+    let target_addition_part = generate_target_addition_part(&input.ident, &builder_ident, &fields);
+    let builder_part = generate_builder_part(&input.vis, &input.ident, &builder_ident, &fields);
+
+    let expanded = quote! {
+        #target_addition_part
+
+        #builder_part
+    };
+
+    expanded.into()
+}
+
+fn analyze_fields(data: &DataStruct) -> Vec<AnalyzedField> {
+    data.fields
         .iter()
         .map(
             |Field {
@@ -41,84 +55,109 @@ pub fn derive(input: TokenStream) -> TokenStream {
                  ty,
                  ..
              }| {
-                let designated_setter_ident = attrs
-                    .first()
-                    .map(|a| {
-                        let meta = a.parse_meta().ok()?;
-                        match meta.path().segments.first()?.ident.to_string().as_str() {
-                            "builder" => {
-                                if let Meta::List(ml) = meta {
-                                    if let NestedMeta::Meta(Meta::NameValue(nv)) =
-                                        ml.nested.first()?
-                                    {
-                                        if nv.path.segments.first()?.ident.to_string().as_str()
-                                            == "each"
-                                        {
-                                            if let Lit::Str(str) = &nv.lit {
-                                                return Some(Ident::new(
-                                                    &str.value(),
-                                                    Span::call_site(),
-                                                ));
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                            _ => {}
-                        }
-                        None
-                    })
-                    .flatten();
+                let name = ident
+                    .clone()
+                    .expect("anonymous field is unsupported")
+                    .to_string();
+                let setter_name = get_designated_setter_name(attrs).unwrap_or(name.clone());
+                let (kind, normalized_type) =
+                    check_special_type(ty).unwrap_or((FieldKind::Normal, ty));
 
-                let setter_ident = designated_setter_ident.map_or(ident.clone(), |i| Some(i));
-
-                if let Type::Path(tp) = ty {
-                    if let Some(seg) = tp.path.segments.first() {
-                        let kind = match seg.ident.to_string().as_str() {
-                            "Option" => Some(FieldKind::Optional),
-                            "Vec" => Some(FieldKind::Multiple),
-                            _ => None,
-                        };
-                        if let Some(kind) = kind {
-                            if let PathArguments::AngleBracketed(ab) = &seg.arguments {
-                                if let Some(GenericArgument::Type(ty)) = ab.args.first() {
-                                    return AnalyzedField {
-                                        vis: vis.clone(),
-                                        ident: ident.clone(),
-                                        normalized_type: ty.clone(),
-                                        kind,
-                                        setter_ident,
-                                    };
-                                }
-                            }
-                        }
-                    }
-                }
                 AnalyzedField {
                     vis: vis.clone(),
-                    ident: ident.clone(),
-                    normalized_type: ty.clone(),
-                    kind: FieldKind::Normal,
-                    setter_ident,
+                    name,
+                    normalized_type: normalized_type.clone(),
+                    kind,
+                    setter_name,
                 }
             },
         )
-        .collect();
+        .collect()
+}
 
+fn get_designated_setter_name(attrs: &Vec<Attribute>) -> Option<String> {
+    let meta = attrs.first()?.parse_meta().ok()?;
+    match meta.path().segments.first()?.ident.to_string().as_str() {
+        "builder" => {
+            if let Meta::List(ml) = meta {
+                if let NestedMeta::Meta(Meta::NameValue(nv)) = ml.nested.first()? {
+                    if &nv.path.segments.first()?.ident.to_string() == "each" {
+                        if let Lit::Str(str) = &nv.lit {
+                            return Some(str.value());
+                        }
+                    }
+                }
+            }
+            None
+        }
+        _ => None,
+    }
+}
+
+fn check_special_type(ty: &Type) -> Option<(FieldKind, &Type)> {
+    if let Type::Path(tp) = ty {
+        let seg = tp.path.segments.first()?;
+        let kind = match seg.ident.to_string().as_str() {
+            "Option" => FieldKind::Optional,
+            "Vec" => FieldKind::Multiple,
+            _ => return None,
+        };
+        if let PathArguments::AngleBracketed(ab) = &seg.arguments {
+            if let GenericArgument::Type(ty) = ab.args.first()? {
+                return Some((kind, ty));
+            }
+        }
+    }
+    None
+}
+
+fn generate_target_addition_part(
+    target_ident: &Ident,
+    builder_ident: &Ident,
+    fields: &Vec<AnalyzedField>,
+) -> TokenStream2 {
+    let field_inits = fields
+        .iter()
+        .map(|AnalyzedField { name, kind, .. }| match kind {
+            FieldKind::Multiple => quote! {
+                #name: vec![]
+            },
+            _ => quote! {
+                #name: None
+            },
+        });
+
+    quote! {
+        impl #target_ident {
+            pub fn builder() -> #builder_ident {
+                #builder_ident {
+                    #(#field_inits,)*
+                }
+            }
+        }
+    }
+}
+
+fn generate_builder_part(
+    target_vis: &Visibility,
+    target_ident: &Ident,
+    builder_ident: &Ident,
+    fields: &Vec<AnalyzedField>,
+) -> TokenStream2 {
     let field_defs = fields.iter().map(
         |AnalyzedField {
              vis,
-             ident,
+             name,
              normalized_type,
              kind,
              ..
          }| {
             match kind {
                 FieldKind::Multiple => quote! {
-                    #vis #ident: Vec<#normalized_type>
+                    #vis #name: Vec<#normalized_type>
                 },
                 _ => quote! {
-                    #vis #ident: Option<#normalized_type>
+                    #vis #name: Option<#normalized_type>
                 },
             }
         },
@@ -127,24 +166,24 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let field_setters = fields.iter().map(
         |AnalyzedField {
              vis,
-             ident,
+             name,
              normalized_type,
-             setter_ident,
+             setter_name,
              kind,
              ..
          }| {
             match kind {
                 FieldKind::Multiple => {
-                    let each = (ident.as_ref().map(|i| i.to_string()) != setter_ident.as_ref().map(|i|i.to_string())).then_some(
+                    let each = (name != setter_name).then_some(
                         quote!{
-                            #vis fn #setter_ident(&mut self, #setter_ident: #normalized_type) -> &mut Self {
-                                self.#ident.push(#setter_ident);
+                            #vis fn #setter_name(&mut self, #setter_name: #normalized_type) -> &mut Self {
+                                self.#name.push(#setter_name);
                                 self
                             }
                         });
                     let direct = quote!{
-                        #vis fn #ident(&mut self, #setter_ident: Vec<#normalized_type>) -> &mut Self {
-                            self.#ident = #setter_ident;
+                        #vis fn #name(&mut self, #setter_name: Vec<#normalized_type>) -> &mut Self {
+                            self.#name = #setter_name;
                             self
                         }
                     };
@@ -156,8 +195,8 @@ pub fn derive(input: TokenStream) -> TokenStream {
                     }
                 }
                 _ => quote! {
-                    #vis fn #ident(&mut self, #ident: #normalized_type) -> &mut Self {
-                        self.#ident = Some(#setter_ident);
+                    #vis fn #name(&mut self, #name: #normalized_type) -> &mut Self {
+                        self.#name = Some(#setter_name);
                         self
                     }
                 },
@@ -167,52 +206,32 @@ pub fn derive(input: TokenStream) -> TokenStream {
 
     let field_guards = fields
         .iter()
-        .map(|AnalyzedField { ident, kind, .. }| match kind {
+        .map(|AnalyzedField { name, kind, .. }| match kind {
             FieldKind::Normal => quote! {
-                let Some(#ident) = self.#ident.clone() else { return Err("".to_string().into()); };
+                let Some(#name) = self.#name.clone() else { return Err(stringify!(#name).to_string().into()); };
             },
             FieldKind::Optional | FieldKind::Multiple => quote! {
-                let #ident = self.#ident.clone();
+                let #name = self.#name.clone();
             },
         });
 
-    let field_idents = fields.iter().map(|f| &f.ident);
-    let field_inits = fields
-        .iter()
-        .map(|AnalyzedField { ident, kind, .. }| match kind {
-            FieldKind::Multiple => quote! {
-                #ident: vec![]
-            },
-            _ => quote! {
-                #ident: None
-            },
-        });
+    let field_idents = fields.iter().map(|f| &f.name);
 
-    let expanded = quote! {
-        #visibility struct #builder_ident {
+    quote! {
+        #target_vis struct #builder_ident {
             #(#field_defs ,)*
         }
 
         impl #builder_ident {
             #(#field_setters)*
 
-            pub fn build(&mut self) -> Result<#ident, Box<dyn std::error::Error>> {
+            pub fn build(&mut self) -> Result<#target_ident, Box<dyn std::error::Error>> {
                 #(#field_guards)*
 
-                Ok(#ident {
+                Ok(#target_ident {
                     #(#field_idents,)*
                 })
             }
         }
-
-        impl Command {
-            pub fn builder() -> #builder_ident {
-                #builder_ident {
-                    #(#field_inits,)*
-                }
-            }
-        }
-    };
-
-    expanded.into()
+    }
 }
