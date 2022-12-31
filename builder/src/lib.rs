df71cd87b759883ed8c30e225ed9006b50d28d9c
MasuqaT <occar@hotmail.co.jp>
2022-12-31 22:12:12 +0900

    pass 02-create-builder

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,38 @@
 use proc_macro::TokenStream;
+use proc_macro2::Span;
+use quote::quote;
+use syn::{Data, DeriveInput, Field, Ident, parse_macro_input};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let input = parse_macro_input!(input as DeriveInput);
+    let visibility = input.vis;
+    let builder_ident = Ident::new(&format!("{}Builder", input.ident), Span::call_site());
+    let Data::Struct(data) = input.data else {
+        return TokenStream::new();
+    };
 
-    TokenStream::new()
+    let field_defs = data.fields.iter().map(|Field { vis, ident, colon_token, ty, .. }| quote! {
+        #vis #ident #colon_token Option<#ty>
+    });
+
+    let field_inits = data.fields.iter().map(|Field { ident, .. }| quote! {
+        #ident: None
+    });
+
+    let expanded = quote! {
+        #visibility struct #builder_ident {
+            #(#field_defs ,)*
+        }
+
+        impl Command {
+            pub fn builder() -> #builder_ident {
+                #builder_ident {
+                    #(#field_inits ,)*
+                }
+            }
+        }
+    };
+
+    expanded.into()
 }
