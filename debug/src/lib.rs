cf80206f3dd81ec9a89869f40979c6ecb79ba8af
MasuqaT <occar@hotmail.co.jp>
2023-01-01 22:15:48 +0900

    pass 02-impl-debug

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,9 +1,27 @@
 use proc_macro::TokenStream;
-use syn::{parse_macro_input, DeriveInput};
+use proc_macro2::Span;
+use quote::quote;
+use syn::{parse_macro_input, Data, DeriveInput};
 
 #[proc_macro_derive(CustomDebug)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = parse_macro_input!(input as DeriveInput);
+    let input = parse_macro_input!(input as DeriveInput);
+    let target_ident = input.ident;
+    let Data::Struct(data) = input.data else {
+        return syn::Error::new(Span::call_site(), "Unsupported".to_string()).into_compile_error().into();
+    };
 
-    TokenStream::new()
+    let fields = data.fields.iter().map(|f| &f.ident);
+
+    let extend = quote! {
+        impl std::fmt::Debug for #target_ident {
+            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::result::Result<(), std::fmt::Error> {
+                f.debug_struct(stringify!(#target_ident))
+                    #( .field(stringify!(#fields), &self.#fields) )*
+                    .finish()
+            }
+        }
+    };
+
+    extend.into()
 }
