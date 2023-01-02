73bd98c5b4d31e0c4930f54d00e5b73bed9509b8
MasuqaT <occar@hotmail.co.jp>
2023-01-02 20:12:31 +0900

    pass 01-parse-headers

--- seq/src/lib.rs
+++ seq/src/lib.rs
@@ -1,8 +1,33 @@
 use proc_macro::TokenStream;
+use syn::parse::{Parse, ParseStream};
+use syn::{braced, parse_macro_input, Token};
 
 #[proc_macro]
 pub fn seq(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let _ = parse_macro_input!(input as SeqInput);
 
-    unimplemented!()
+    TokenStream::new()
+}
+
+struct SeqInput {
+    var: syn::Ident,
+    in_token: Token![in],
+    start: syn::LitInt,
+    range_token: Token![..],
+    end: syn::LitInt,
+    brace_token: syn::token::Brace,
+}
+
+impl Parse for SeqInput {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        let content;
+        Ok(SeqInput {
+            var: input.parse()?,
+            in_token: input.parse()?,
+            start: input.parse()?,
+            range_token: input.parse()?,
+            end: input.parse()?,
+            brace_token: braced!(content in input),
+        })
+    }
 }
