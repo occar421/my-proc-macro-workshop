63a6ce95156de38e2ceff71e4c71898d50f29bcf
MasuqaT <occar@hotmail.co.jp>
2023-01-02 21:43:22 +0900

    temp 03

--- seq/src/lib.rs
+++ seq/src/lib.rs
@@ -1,23 +1,55 @@
 use proc_macro::TokenStream;
-use syn::__private::TokenStream2;
+use proc_macro2::{Group, Ident, Literal, TokenStream as TokenStream2, TokenTree};
+use quote::quote;
 use syn::parse::{Parse, ParseStream};
-use syn::{braced, parse_macro_input, Token};
+use syn::token::Brace;
+use syn::{braced, parse_macro_input, LitInt, Token};
 
 #[proc_macro]
 pub fn seq(input: TokenStream) -> TokenStream {
-    let _ = parse_macro_input!(input as SeqInput);
+    let input = parse_macro_input!(input as SeqInput);
 
-    TokenStream::new()
+    let start = match input.start.base10_parse::<usize>() {
+        Ok(x) => x,
+        Err(x) => return x.into_compile_error().into(),
+    };
+
+    let end = match input.end.base10_parse::<usize>() {
+        Ok(x) => x,
+        Err(x) => return x.into_compile_error().into(),
+    };
+
+    let generated_codes = (start..end).map(|i| replace(input.body.clone(), (input.var.clone(), i)));
+
+    (quote! {
+        #(#generated_codes)*
+    })
+    .into()
 }
 
+fn replace(ts: TokenStream2, var: (Ident, usize)) -> TokenStream2 {
+    ts.into_iter()
+        .map(|t| match t {
+            TokenTree::Group(g) => {
+                TokenTree::Group(Group::new(g.delimiter(), replace(g.stream(), var.clone())))
+            }
+            TokenTree::Ident(ident) if ident.to_string() == var.0.to_string() => {
+                TokenTree::Literal(Literal::usize_unsuffixed(var.1))
+            }
+            x => x,
+        })
+        .collect()
+}
+
+#[derive(Debug)]
 struct SeqInput {
-    var: syn::Ident,
+    var: Ident,
     in_token: Token![in],
-    start: syn::LitInt,
+    start: LitInt,
     range_token: Token![..],
-    end: syn::LitInt,
-    brace_token: syn::token::Brace,
-    tokens: TokenStream2,
+    end: LitInt,
+    brace_token: Brace,
+    body: TokenStream2,
 }
 
 impl Parse for SeqInput {
@@ -30,7 +62,7 @@ impl Parse for SeqInput {
             range_token: input.parse()?,
             end: input.parse()?,
             brace_token: braced!(content in input),
-            tokens: content.parse()?,
+            body: content.parse()?,
         })
     }
 }
