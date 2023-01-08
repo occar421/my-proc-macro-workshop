a7b4d98eda62b1ee5459910b0c2443aadf1277eb
MasuqaT <occar@hotmail.co.jp>
2023-01-09 02:44:24 +0900

    temp 05(2)

--- bitfield/impl/src/lib.rs
+++ bitfield/impl/src/lib.rs
@@ -17,67 +17,57 @@ pub fn bitfield(args: TokenStream, input: TokenStream) -> TokenStream {
 
     let name = item.ident;
 
-    let field_lengths: Vec<_> = item
-        .fields
-        .iter()
-        .map(|f| match &f.ty {
-            syn::Type::Path(tp) => &tp.path,
-            _ => unimplemented!(),
-        })
-        .map(|p| &p.segments.last().unwrap().ident)
-        .filter_map(|i| {
-            i.to_string()
-                .strip_prefix("B")
-                .map(|s| s.parse::<usize>().ok())
-                .flatten()
-        })
-        .collect();
-    // FIXME: `field_range` は type alias が出てきて使い物にならないので、方法を見直す
-    // CG 演算 に加えて、a b c d それぞれの length offset Phantom タイプ？
-
     let field_names = item.fields.iter().filter_map(|f| f.ident.as_ref());
-    let field_range = field_lengths.iter().scan(0, |offset, length| {
-        let current_offset = offset.clone();
-        *offset += length;
-        Some((current_offset, length))
-    });
-
-    let accessors = field_names
-        .zip(field_range)
-        .into_iter()
-        .map(|(i, (offset, length))| {
-            let getter_name = syn::Ident::new(&format!("get_{}", i), i.span());
-            let setter_name = syn::Ident::new(&format!("set_{}", i), i.span());
-
-            quote! {
-                pub fn #getter_name(&self) -> u64 {
-                    let mut array_be = [0; 8];
-                    for i in 0..#length {
-                        let data_i = i + #offset;
-                        let array_i = i + 64 - #length;
-                        if (self.data[data_i / 8] & (0x1 << ((8 - (data_i % 8)) % 8)) > 0) {
-                            array_be[array_i / 8] |= 0x1 << ((8 - (array_i % 8)) % 8);
-                        }
-                    }
-                    u64::from_be_bytes(array_be)
-                }
-
-                pub fn #setter_name(&mut self, value: u64) {
-                    let array_be = u64::to_be_bytes(value);
-                    for i in 0..#length {
-                        let data_i = i + #offset;
-                        let array_i = i + 64 - (#offset + #length);
-                        self.data[data_i / 8] &= !(0x1 << (data_i % 8)); // reset
-                        self.data[data_i / 8] |= array_be[array_i / 8] & (0x1 << (array_i % 8));
-                    }
-                }
-            }
-        });
-
     let type_paths = item.fields.iter().map(|f| match &f.ty {
         syn::Type::Path(tp) => tp,
         _ => unimplemented!(),
     });
+    let accessors =
+        field_names
+            .zip(type_paths.clone())
+            .into_iter()
+            .scan(quote! { 0 }, |prev, (i, tp)| {
+                let getter_name = syn::Ident::new(&format!("get_{}", i), i.span());
+                let setter_name = syn::Ident::new(&format!("set_{}", i), i.span());
+
+                let length = quote! {
+                    #tp::BITS
+                };
+
+                let offset = quote! {
+                    #prev
+                };
+
+                let accessor = quote! {
+                    pub fn #getter_name(&self) -> u64 {
+                        let mut array_be = [0; 8];
+                        for i in 0..#length {
+                            let data_i = i + #offset;
+                            let array_i = i + 64 - #length;
+                            if (self.data[data_i / 8] & (0x1 << ((8 - (data_i % 8)) % 8)) > 0) {
+                                array_be[array_i / 8] |= 0x1 << ((8 - (array_i % 8)) % 8);
+                            }
+                        }
+                        u64::from_be_bytes(array_be)
+                    }
+
+                    pub fn #setter_name(&mut self, value: u64) {
+                        let array_be = u64::to_be_bytes(value);
+                        for i in 0..#length {
+                            let data_i = i + #offset;
+                            let array_i = i + 64 - (#offset + #length);
+                            self.data[data_i / 8] &= !(0x1 << (data_i % 8)); // reset
+                            self.data[data_i / 8] |= array_be[array_i / 8] & (0x1 << (array_i % 8));
+                        }
+                    }
+                };
+
+                *prev = quote! {
+                    (#offset + #length)
+                };
+
+                Some(accessor)
+            });
 
     let n_bits = quote! {
         (#(#type_paths::BITS)+*)
@@ -91,7 +81,6 @@ pub fn bitfield(args: TokenStream, input: TokenStream) -> TokenStream {
        #[repr(C)]
         pub struct #name {
             data: [u8; #n_bytes],
-            // a: PhantomData<CG2<0, A::BITS>>, FIXME: PhantomData では TS のような #name.a への型アクセスはできないので NG
         }
 
         impl #name where
