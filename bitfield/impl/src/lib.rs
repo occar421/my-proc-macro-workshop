4fb654058a4863a8f3d37c8c8d997065ee627ebc
MasuqaT <occar@hotmail.co.jp>
2023-01-08 21:10:39 +0900

    temp 03(2)

--- bitfield/impl/src/lib.rs
+++ bitfield/impl/src/lib.rs
@@ -56,52 +56,87 @@ pub fn bitfield(args: TokenStream, input: TokenStream) -> TokenStream {
             let array_most = 8 - data_byte_length;
             let array_least = 7usize;
 
-            // ║ most_padding | ~ | least_padding ║
-            let most_padding = offset % 8;
-            let least_padding = 8 - (offset + length - data_least * 8);
+            // ║ most_byte_padding | ~ | least_byte_padding ║
+            let most_byte_padding = offset % 8;
+            let most_byte_content_mask: u8 = !0 << most_byte_padding >> most_byte_padding;
+            let least_byte_padding = 8 - (offset + length - data_least * 8);
+            let least_byte_content_mask:u8 = !0 >> least_byte_padding << least_byte_padding;
+
+            let array_to_data_offset = least_byte_padding;
+            let array_to_data_offset_stored_mask: u8 = if array_to_data_offset == 0 {
+                0
+            }  else {
+                !0 >> (8 - array_to_data_offset) << (8 - array_to_data_offset)
+            };
 
             let getter_logic = if data_byte_length <= 1 {
                 quote! {
                     let mut array_be = [0; 8];
 
-                    array_be[#array_least] = (self.data[#data_least] & (!0 << #most_padding >> #most_padding)) >> #least_padding;
+                    array_be[#array_least] = (self.data[#data_least] & #most_byte_content_mask) >> #array_to_data_offset;
 
                     u64::from_be_bytes(array_be)
                 }
             } else {
-                quote! {
-                    let mut array_be = [0; 8];
+                if array_to_data_offset == 0 {
+                    quote! {
+                        let mut array_be = [0; 8];
 
-                    for i in 0..#data_byte_length {
-                        array_be[#array_least - i] =
-                            ((self.data[#data_least - 1 - i] & (!0 >> #least_padding)) << #least_padding)
-                            + (self.data[#data_least - i] >> #least_padding);
+                        for i in 0..#data_byte_length {
+                            array_be[#array_least - i] =self.data[#data_least - i];
+                        }
+                        array_be[#array_most] &= #most_byte_content_mask;
+
+                        u64::from_be_bytes(array_be)
                     }
-                    array_be[#array_most] &= ((!0 << #most_padding >> #most_padding) >> #least_padding);
+                } else {
+                    quote! {
+                        let mut array_be = [0; 8];
 
-                    u64::from_be_bytes(array_be)
+                        for i in 0..#data_byte_length {
+                            array_be[#array_least - i] =
+                                ((self.data[#data_least - 1 - i] & (!0 >> #array_to_data_offset)) << #array_to_data_offset)
+                                + (self.data[#data_least - i] >> #array_to_data_offset);
+                        }
+                        array_be[#array_most] &= (#most_byte_content_mask >> #array_to_data_offset);
+                        dbg!(&array_be);
+
+                        u64::from_be_bytes(array_be)
+                    }
                 }
             };
 
             let setter_logic = if data_byte_length <= 1 {
                 quote! {
                     let array_be = u64::to_be_bytes(value);
-                    let stored = self.data[#array_least] & !((!0 << #most_padding >> #most_padding) & (!0 >> right_padding << right_padding));
-                    let value = (array_be[#array_least] & (!0 << #most_padding >> #most_padding)) << #least_padding;
-                    self.data[#array_least] = stored | value;
+                    let stored = self.data[#data_least] & !(#most_byte_content_mask & #least_byte_content_mask);
+                    let value = (array_be[#array_least] & #most_byte_content_mask) << #array_to_data_offset;
+                    self.data[#data_least] = stored | value;
                 }
             } else {
-                quote! {
-                    let array_be = u64::to_be_bytes(value);
+                if array_to_data_offset == 0 {
+                    quote! {
+                        let array_be = u64::to_be_bytes(value);
 
-                    // for i in 0..#data_byte_length {
-                    //     self.data[#data_least - i]
-                    // }
-                    let stored = self.data[#array_least] & (^0 >> (8 - #least_padding) << (8 - #least_padding));
-                    self.data[#data_least] = array_be[#array_least] << #least_padding + stored;
+                        for i in 0..#data_byte_length {
+                            self.data[#data_least] = array_be[#array_least - i];
+                        }
 
-                    let stored = self.data[#array_least - 1] & (^0 >> (8 - #least_padding) << (8 - #least_padding));
-                    self.data[#data_least] = array_be[#array_least] << #least_padding + stored
+                        let stored = self.data[#data_most] & !#most_byte_content_mask;
+                        self.data[#data_most] = (array_be[#array_most] & #most_byte_content_mask) | stored;
+                    }
+                } else {
+                    quote! {
+                        let array_be = u64::to_be_bytes(value);
+
+                        for i in 0..#data_byte_length {
+                            let stored = self.data[#data_least - i] & #array_to_data_offset_stored_mask;
+                            self.data[#data_least] = (array_be[#array_least - i] << #array_to_data_offset) | stored;
+                        }
+
+                        let stored = self.data[#data_most] & (!#most_byte_content_mask | #array_to_data_offset_stored_mask);
+                        self.data[#data_most] = ((array_be[#array_most] & #most_byte_content_mask) << #array_to_data_offset) | stored;
+                    }
                 }
             };
 
